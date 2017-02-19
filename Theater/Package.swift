93258e7e4e90c798f719888ead1977a3978349e2
Haichuan Wang <Haichuan.Wang@huawei.com>
2017-02-19 11:57:04 -0800

    Use the latest Theater version

--- Theater/Package.swift
+++ Theater/Package.swift
@@ -1,12 +1,12 @@
 import PackageDescription
 
 let package = Package(
-   name: "Benchmark",
-   targets: [],
-   dependencies: [
-	   .Package(url: "git@github.com:Huawei-PTLab/Theater.git", versions: Version(1,2,2)..<Version(2,0,0)),
-   ]
-   )
+    name: "Benchmark",
+    targets: [],
+    dependencies: [
+        .Package(url: "http://github.com/Huawei-PTLab/Theater.git", versions: Version(1,2,3)..<Version(2,0,0)),
+    ]
+)
 
 let targetRing = Target(name: "Ring")
 let targetRing2 = Target(name: "Ring2")
