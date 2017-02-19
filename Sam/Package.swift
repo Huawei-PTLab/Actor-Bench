63504bf6515c517b8a039fc10cce55b64b30b779
Haichuan Wang <Haichuan.Wang@huawei.com>
2017-02-19 11:47:31 -0800

    Change package url to http protocal

--- Sam/Package.swift
+++ Sam/Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Sam-Bench",
     dependencies: [
-        .Package(url: "git@github.com:Huawei-PTLab/Sam.git", majorVersion: 0, minor: 1)
+        .Package(url: "http://github.com/Huawei-PTLab/Sam.git", majorVersion: 0, minor: 1)
     ]
 )
