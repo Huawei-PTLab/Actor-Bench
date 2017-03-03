f98d062feee18b991b5a34777f4fa27650253829
Bryan Chan <bryan.chan@huawei.com>
2017-03-03 00:38:40 -0500

    Rename "Ring" to "BusyRing", and "Ring2" to "Ring" (Akka only at this time); also modify the latter to match its description in the literature.

--- Akka/src/main/scala/BusyRing.scala
+++ Akka/src/main/scala/BusyRing.scala
@@ -1,16 +1,16 @@
 import java.util.Calendar
 import akka.actor._
 
-object Ring {
-  val system = ActorSystem("Ring")
+object BusyRing {
+  val system = ActorSystem("BusyRing")
   var startTime: Long = 0
   var endTime: Long = 0
 
   def main(args: Array[String]): Unit = {
     val numNodes = args(0).toInt
-    val numRounds = args(1).toInt
+    val numMessages = args(1).toInt
     val nodes =
-      for (i <- 0 until numNodes) yield system.actorOf(Props(classOf[RingNode], i, numRounds), "Node" + i)
+      for (i <- 0 until numNodes) yield system.actorOf(Props(classOf[RingNode], i, numMessages), "Node" + i)
     for (i <- 0 until numNodes) nodes(i) ! Connect(nodes((i + 1) % numNodes))
     nodes(0) ! Start
   }
@@ -18,10 +18,11 @@ object Ring {
   case object Start
   case object Stop
   case class Connect(next: ActorRef)
-  case class Token(goal: Int, lapsRemaining: Int)
+  case class Token(goal: Int)
 
-  class RingNode(val nodeId: Int, val numRounds: Int) extends Actor {
+  class RingNode(val nodeId: Int, val numMessages: Int) extends Actor {
     var nextNode: ActorRef = context.system.deadLetters
+    var completed: Int = 0
 
     def receive = {
       case Connect(next: ActorRef) =>
@@ -30,21 +31,22 @@ object Ring {
 
       case Start =>
         println("Start: \t" + Calendar.getInstance().getTime)
-        Ring.startTime = System.currentTimeMillis()
-        nextNode ! Token(nodeId, numRounds)
+        BusyRing.startTime = System.currentTimeMillis()
+        1 to numMessages foreach { _ =>
+          nextNode ! Token(nodeId)
+        }
 
-      case Token(goal, lapsRemaining) =>
+      case Token(goal) =>
         if (goal == nodeId) {
-          if (lapsRemaining == 1) {
-            Ring.endTime = System.currentTimeMillis()
+          completed += 1
+          if (completed == numMessages) {
+            BusyRing.endTime = System.currentTimeMillis()
             println("Stop: \t" + Calendar.getInstance().getTime)
-            println(s"Elapsed time: ${(Ring.endTime - Ring.startTime) / 1000.0}s")
+            println(s"Elapsed time: ${(BusyRing.endTime - BusyRing.startTime) / 1000.0}s")
             system.shutdown()
-          } else {
-            nextNode ! Token(goal, lapsRemaining - 1)
           }
         } else {
-          nextNode ! Token(goal, lapsRemaining)
+          nextNode ! Token(goal)
         }
     }
   }
